84f14cc0da6ab3ad6704b1d02955199999e3c476
Thiago Bianeck <thiagobianeck@gmail.com>
2019-10-06 11:55:46 -0300

    Finalizado o repositório do usuário

---
+++ src/main/java/com/wallet/entity/User.java
@@ -0,0 +1,23 @@
+package com.wallet.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+}
---
+++ src/main/java/com/wallet/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.wallet.repository;
+
+import com.wallet.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailEquals(String email);
+}
