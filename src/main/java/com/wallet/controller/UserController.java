1ace6897d17e3af42d78d5153a24989d2e20fcb0
Thiago Bianeck <thiagobianeck@gmail.com>
2019-10-07 22:35:18 -0300

    Configurando o Flyway

--- src/main/java/com/wallet/controller/UserController.java
+++ src/main/java/com/wallet/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +24,14 @@ public class UserController {
     private UserService service;
 
     @PostMapping
-//    public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO dto, BindResult result) {
-    public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO dto) {
+    public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO dto, BindingResult result) {
+//    public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO dto) {
         Response<UserDTO> response = new Response<UserDTO>();
+
+        if(result.hasErrors()) {
+            result.getAllErrors().forEach(e -> response.getErrors().add(e.getDefaultMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
 
         User user = service.save(this.convertDtoToEntity(dto));
 
@@ -37,6 +43,7 @@ public class UserController {
 
     private User convertDtoToEntity(UserDTO dto) {
         User u = new User();
+        u.setId(dto.getId());
         u.setEmail(dto.getEmail());
         u.setName(dto.getName());
         u.setPassword(dto.getPassword());
@@ -46,6 +53,7 @@ public class UserController {
 
     private UserDTO convertEntityToDto(User user) {
         UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
         dto.setEmail(user.getEmail());
         dto.setName(user.getName());
         dto.setPassword(user.getPassword());
