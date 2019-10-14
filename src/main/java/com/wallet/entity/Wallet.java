60fd30d471529079b19407861ae75f43da80b5c0
Thiago Bianeck <thiagobianeck@gmail.com>
2019-10-14 12:01:09 -0300

    Criando Wallet

---
+++ src/main/java/com/wallet/entity/Wallet.java
@@ -0,0 +1,28 @@
+package com.wallet.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Wallet implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private BigDecimal value;
+
+
+}
