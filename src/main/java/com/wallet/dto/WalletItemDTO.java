40537fb379cbeff35217d539ecd184f5631c1c81
Thiago Bianeck <thiagobianeck@gmail.com>
2019-10-23 23:42:46 -0300

    WalletItens

--- src/main/java/com/wallet/dto/WalletItemDTO.java
+++ src/main/java/com/wallet/dto/WalletItemDTO.java
@@ -1,5 +1,6 @@
 package com.wallet.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -15,6 +16,7 @@ public class WalletItemDTO {
     @NotNull(message = "Insira o id da carteira")
     private Long wallet;
     @NotNull(message = "Informe uma data")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy", locale = "pt-BR", timezone = "Brazil/East")
     private Date date;
     @NotNull(message = "Informe um tipo")
     @Pattern(regexp = "^(ENTRADA|SAÍDA)$", message = "Para o tipo somente são aceitos os valores ENTRADA ou SAÍDA")
--- src/main/java/com/wallet/repository/WalletItemRepository.java
+++ src/main/java/com/wallet/repository/WalletItemRepository.java
@@ -1,17 +1,17 @@
 package com.wallet.repository;
 
-import com.wallet.entity.WalletItem;
-import com.wallet.util.enums.TypeEnum;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
+import com.wallet.entity.WalletItem;
+import com.wallet.util.enums.TypeEnum;
 
 public interface WalletItemRepository extends JpaRepository<WalletItem, Long> {
     Page<WalletItem> findAllByWalletIdAndDateGreaterThanEqualAndDateLessThanEqual(Long wallet, Date init, Date end, Pageable page);
